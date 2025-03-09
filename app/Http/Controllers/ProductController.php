4a1d8611aa41a53c296cad45b5558c0b83149cca
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-09 18:27:34 +0500

    created two controlller one for controller users anther one for controller product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductsRequest;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('product');
+    }
+
+    public function store(StoreProductsRequest $request)
+    {
+        return request()->all();
+    }
+
+    /**
+     */
+    public function show(string $id)
+    {
+        
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
